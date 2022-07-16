fab34cccd3ea171dd8accdf6e5bbab458c4a0e43
Federico Dionisi <code@fdionisi.me>
2022-07-16 17:02:38 +0200

    temp

---
+++ crates/cli/README.md
@@ -0,0 +1,2 @@
+Until headless_chrome publishes a new release you should build fast-cli from code.
+Clone and build from https://github.com/fdionisi/fast-cli
--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -1,4 +1,4 @@
 fn main() {
     println!("Until headless_chrome publishes a new release you should build fast-cli from code.");
-    println!("Clone and build https://github.com/fdionisi/fast-cli");
+    println!("Clone and build from https://github.com/fdionisi/fast-cli");
 }
